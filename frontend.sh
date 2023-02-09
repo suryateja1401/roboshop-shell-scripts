b4ea523dcbeadadef19d064d8f478a4443eef086
TEJA <suryateja1401@gmail.com>
2023-02-09 07:09:20 +0530

    Adding redirectories to frontend script

--- frontend.sh
+++ frontend.sh
@@ -1,22 +1,22 @@
 echo installing nginx
-yum install nginx -y & >>/tmp/frontend
+yum install nginx -y &>>/tmp/frontend
 
 echo Downloading nginx web content
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" & >>/tmp/frontend
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/frontend
 
 echo Redirecting to nginx folder
-cd /usr/share/nginx/html & >>/tmp/frontend
+cd /usr/share/nginx/html &>>/tmp/frontend
 
 echo Removing Old web content
- rm -rf * & >>/tmp/frontend
+ rm -rf * &>>/tmp/frontend
 
 echo Extracting nginx web content
-unzip /tmp/frontend.zip & >>/tmp/frontend
+unzip /tmp/frontend.zip &>>/tmp/frontend
 
 echo Renaming and moving files and folders
-mv frontend-main/static/* . & >>/tmp/frontend
-mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf & >>/tmp/frontend
+mv frontend-main/static/* . &>>/tmp/frontend
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/frontend
 
 echo Starting nginx services
-systemctl enable nginx & >>/tmp/frontend
-systemctl restart nginx& >>/tmp/frontend
+systemctl enable nginx &>>/tmp/frontend
+systemctl restart nginx&>>/tmp/frontend

6dc4eb1c469c773ff5d8e79062870ab4fdec4e00
TEJA <suryateja1401@gmail.com>
2023-02-14 07:06:49 +0530

    monogodb script

--- mongodb.sh
+++ mongodb.sh
@@ -15,3 +15,22 @@ echo starting mongodb
 systemctl enable mongod &>>LOG_FILE
 systemctl start mongod &>>LOG_FILE
 echo status $?
+
+echo Downloading Schema
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"&>>LOG_FILE
+echo status $?
+
+cd /tmp
+echo Extract Schema file
+unzip mongodb.zip&>>LOG_FILE
+echo status $?
+
+cd  mongodb-main
+
+echo load catalogue schema file
+mongo < catalogue.js&>>LOG_FILE
+echo status $?
+
+echo load service schema file
+mongo < users.js&>>LOG_FILE
+echo status $?

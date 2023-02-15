aac7f698411a4d31811ba810618472a32797fdc8
TEJA <suryateja1401@gmail.com>
2023-02-15 06:33:06 +0530

    catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -1,16 +1,39 @@
 LOG_FILE=/tmp/catalogue
 
 echo Create catalogue repo
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
-# yum install nodejs -y
-# useradd roboshop
-$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
-$ cd /home/roboshop
-$ unzip /tmp/catalogue.zip
-$ mv catalogue-main catalogue
-$ cd /home/roboshop/catalogue
-$ npm install
-# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-# systemctl daemon-reload
-# systemctl start catalogue
-# systemctl enable catalogue
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>LOG_FILE
+echo status $?
+
+echo Installing Nodejs
+yum install nodejs -y &>>LOG_FILE
+echo status $?
+
+echo Adding Roboshop Application User
+useradd roboshop &>>LOG_FILE
+echo status $?
+
+echo Downloading Catalogue Applicaton code
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>LOG_FILE
+echo status $?
+
+cd /home/roboshop &>>LOG_FILE
+
+echo Extracting Catalogue Application code
+unzip /tmp/catalogue.zip &>>LOG_FILE
+echo status $?
+
+mv catalogue-main catalogue &>>LOG_FILE
+cd /home/roboshop/catalogue &>>LOG_FILE
+
+echo Installing Nodejs Dependencies
+npm install &>>LOG_FILE
+echo status $?
+
+echo Setup catalogue services
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>LOG_FILE
+echo status $?
+
+echo start catalogue services
+systemctl start catalogue &>>LOG_FILE
+ystemctl enable catalogue &>>LOG_FILE
+echo ststus $?

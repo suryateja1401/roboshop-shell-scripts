a68c565ba979bc563b27dc3110d10036383aef20
TEJA <suryateja1401@gmail.com>
2023-02-17 07:19:43 +0530

    functions

--- frontend.sh
+++ frontend.sh
@@ -1,30 +1,32 @@
 LOG_FILE=/tmp/frontend
-echo installing nginx
+
+source common.sh
+
 yum install nginx -y &>>$LOG_FILE
-echo status $?
+statuscheck $?
 
 echo Downloading nginx web content
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
-echo status $?
+statuscheck $?
 
 echo Redirecting to nginx folder
 cd /usr/share/nginx/html &>>$LOG_FILE
-echo status $?
+statuscheck $?
 
 echo Removing Old web content
  rm -rf * &>>$LOG_FILE
-echo status $?
+statuscheck $?
 
 echo Extracting nginx web content
 unzip /tmp/frontend.zip &>>$LOG_FILE
-echo status $?
+statuscheck $?
 
 echo Renaming and moving files and folders
 mv frontend-main/static/* . &>>$LOG_FILE
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
-echo status $?
+statuscheck $?
 
 echo Starting nginx services
 systemctl enable nginx &>>$LOG_FILE
 systemctl restart nginx&>>$LOG_FILE
-echo status $?
+statuscheck $?

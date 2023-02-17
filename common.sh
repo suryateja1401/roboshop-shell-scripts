0a8ade1326f8b0b79a60653c22913276774d8346
TEJA <suryateja1401@gmail.com>
2023-02-17 07:13:08 +0530

    functions

--- common.sh
+++ common.sh
@@ -3,7 +3,7 @@ if [ $ID -ne 0 ]; then
   echo You should execute code as root user or with sudo previlages
 fi
 
-status check(){
+statuscheck() {
   if [ $1 -eq 0 ]; then
     echo -e status = "\e[32mSuccess\e[0m"
     else

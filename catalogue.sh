289288160064ab795304a0fec22992f0409e648a
TEJA <suryateja1401@gmail.com>
2023-02-17 07:15:33 +0530

    functions

--- catalogue.sh
+++ catalogue.sh
@@ -4,45 +4,45 @@ source common.sh
 
 echo Create catalogue repo
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
-status check $?
+statuscheck $?
 
 echo Installing Nodejs
 yum install nodejs -y &>>$LOG_FILE
-status check $?
+statuscheck $?
 
 id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]; then
     echo Adding Roboshop Application User
     useradd roboshop &>>$LOG_FILE
-  status check $?
+  statuscheck $?
    fi
 
 echo Downloading Catalogue Applicaton code
 curl -s -L -o  /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG_FILE
-status check $?
+statuscheck $?
 
 cd /home/roboshop
 echo Clean Old App Content
 rm -rf catalogue &>>$LOG_FILE
-status check $?
+statuscheck $?
 
 
 echo Extracting Catalogue Application code
 unzip /tmp/catalogue.zip &>>$LOG_FILE
-status check $?
+statuscheck $?
 
 mv catalogue-main catalogue &>>$LOG_FILE
 cd /home/roboshop/catalogue &>>$LOG_FILE
 
 echo Installing Nodejs Dependencies
 npm install &>>$LOG_FILE
-status check $?
+statuscheck $?
 
 echo Setup catalogue services
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>$LOG_FILE
-status check $?
+statuscheck $?
 
 echo start catalogue services
 systemctl start catalogue &>>$LOG_FILE
 systemctl enable catalogue &>>$LOG_FILE
-status check $?
+statuscheck $?

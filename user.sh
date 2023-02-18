15d4c9ec2e056fa62531ae690e8f19eb1872bb5c
TEJA <suryateja1401@gmail.com>
2023-02-18 06:49:11 +0530

    user

--- user.sh
+++ user.sh
@@ -5,11 +5,34 @@ source common.sh
 echo create user repo
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
 statuscheck $?
-# yum install nodejs -y &>>$LOG_FILE
-useradd roboshop &>>$LOG_FILE
-$ curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip" &>>$LOG_FILE
-$ cd /home/roboshop &>>$LOG_FILE
-$ unzip /tmp/user.zip &>>$LOG_FILE
-$ mv user-main user &>>$LOG_FILE
-$ cd /home/roboshop/user &>>$LOG_FILE
-$ npm install &>>$LOG_FILE
+
+echo Installing Nodejs
+yum install nodejs -y &>>$LOG_FILE
+statuscheck $?
+
+id roboshop &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    echo Adding Roboshop Application User
+    useradd roboshop &>>$LOG_FILE
+  statuscheck $?
+   fi
+
+echo Downloading user Applicaton code
+curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip" &>>$LOG_FILE
+statuscheck $?
+
+cd /home/roboshop &>>$LOG_FILE
+echo Clean Old App Content
+rm -rf catalogue &>>$LOG_FILE
+statuscheck $?
+
+echo Extracting user Application code
+unzip -o /tmp/user.zip &>>$LOG_FILE
+statuscheck $?
+
+mv user-main user &>>$LOG_FILE
+cd /home/roboshop/user &>>$LOG_FILE
+
+echo Installing Nodejs Dependencies
+npm install &>>$LOG_FILE
+statuscheck $?

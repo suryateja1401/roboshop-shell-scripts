f933a8308fd5319d338a73311a3d1462adeb2f73
TEJA <suryateja1401@gmail.com>
2023-02-22 12:22:39 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -21,7 +21,8 @@ systemctl restart mysqld  &>>$LOG_FILE
 statuscheck $?
 
 DEFAULT_PASSWORD=$( sudo grep 'temporary password'  /var/log/mysqld.log |awk '{print $NF}')
-echo "SET PASSWORD FOR 'root'@'localhost' =PASSWORD {'${ROBOSHOP_MYSQL_PASSWORD}'}; FLUSH PREVILEGES;" >/tmp/root-pass.sql
+echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('mypass');
+      FLUSH PRIVILEGES;" >/tmp/root-pass.sql
 
 echo show databases
 mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD}  &>>$LOG_FILE

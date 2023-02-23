8a14f0721de9d2270d7beb9a049aa2cb1ab56bc4
TEJA <suryateja1401@gmail.com>
2023-02-23 06:53:33 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -31,10 +31,10 @@ if [ $? -ne is 0 ];then
   statuscheck $?
 fi
 
+echo 'show plugins'| mysql -uroot -pRoboShop@1 | grep  validate_password &>>$LOG_FILE
 
-# grep temp /var/log/mysqld.log  &>>$LOG_FILE
-# mysql_secure_installation  &>>$LOG_FILE
-# mysql -uroot -pRoboShop@1  &>>$LOG_FILE
+
+
 # unin#install plugin validate_password  &>>$LOG_FILE
 # curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"  &>>$LOG_FILE
 # cd /tmp  &>>$LOG_FILE
